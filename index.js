127e424d1c00ff75de8c3ab2fba01bcb85910289
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-02-16 19:38:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+/*The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array */ 
+const dogArray = ['Bark', 'Beep', 'Boop'];
+console.log(dogArray.unshift('Fido', 'Zimba'));  //method always has a call on it that returns the length of the array.
+console.log(dogArray);
