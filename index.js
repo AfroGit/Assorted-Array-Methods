ef5abaefa607be80288377ab52ac56548cbf22f2
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-02-16 20:51:13 +0000

    Array manipulation using  push() and shift() line 19 & 20.

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 /*The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array */ 
-const dogArray = ['Bark', 'Beep', 'Boop'];
-console.log(dogArray.unshift('Fido', 'Zimba'));  //method always has a call on it that returns the length of the array.
-console.log(dogArray);
+// const dogArray = ['Bark', 'Beep', 'Boop'];
+// console.log(dogArray.unshift('Fido', 'Zimba'));  //method always has a call on it that returns the length of the array.
+// console.log(dogArray);
+
+// ----------------------------------------------------
+
+/*
+
+Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+Add the number to the end of the array, then remove the first element of the array.
+
+The nextInLine function should then return the element that was removed.
+ */ 
+
+// function nextInLine(arr, item) {
+  // Only change code below this line
+  // arr.push(item);
+  // return arr.shift();
+  // Only change code above this line
+};
+// nextInLine([1, 3, 7], 5);
+ // console.log(nextInLine([1, 2, 3, 4, 5], 7));
+// console.log(nextInLine.length);
