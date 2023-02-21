08026027d9b7e2bea74ae902b1ce8351d45cd2cd
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-02-21 22:42:37 +0000

    Using switch statements to make a card counting game

--- index.js
+++ index.js
@@ -87,9 +87,48 @@ Your function will be passed par and strokes arguments. Return the correct strin
 
 // -----------------------------------------------
 
-function myFun() {
-  console.log("Hello");
-   return "World";// The string byebye will never display in the console, because the function exits at the return statement.
-  console.log("byebye")
+// function myFun() {
+//   console.log("Hello");
+//    return "World";// The string byebye will never display in the console, because the function exits at the return statement.
+//   console.log("byebye")
+// }
+// myFun();
+
+// -----------------------------------------------------
+/*In the casino game Blackjack, a player can determine whether they have an advantage on the next hand over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+
+Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low. */ 
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch(card){
+
+case 2:
+case 3:
+case 4:
+case 5:
+case 6:
+count++;
+break;
+
+case 10:
+case 'J':
+case 'Q':
+case 'K':
+case 'A':
+count--;
+break;
+
 }
-myFun();
+if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+
+}
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+console.log(cc(7));
