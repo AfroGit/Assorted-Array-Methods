18f6dc231a18e874ed7ab39266b88d67c10213db
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-10 19:09:28 +0000

    indexOf( ) takes an element as a parameter, and when called, it returns the position, or index, of that element, or -1 if the element does not exist on the array. Ln 237

--- index.js
+++ index.js
@@ -220,10 +220,25 @@ Having more high cards remaining in the deck favors the player. Each card is ass
 /* defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun']. */ 
 
 
-function spreadOut() {
-  let fragment = ['to', 'code'];
-  let sentence = ['learning', ... fragment, 'is', 'fun']; // Change this line
-  return sentence;
-}
+// function spreadOut() {
+//   let fragment = ['to', 'code'];
+//   let sentence = ['learning', ... fragment, 'is', 'fun']; // Change this line
+//   return sentence;
+// }
 
-console.log(spreadOut());
+// console.log(spreadOut());
+
+// -----------------------------------------------
+/* Define a function, quickCheck, that takes an array and an element as arguments. Modify the function using indexOf() so that it returns true if the passed element exists on the array, and false if it does not. */ 
+
+
+function quickCheck(arr, elem) {
+ 
+   if(arr.indexOf(elem) >= 0){
+       return true;
+} 
+       return false;
+  
+};
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
