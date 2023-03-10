5e5fa25c4f94bed632f33d77bf80d1b05fdba176
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-10 18:45:22 +0000

    Another huge advantage of the spread operator is the ability to combine arrays, or to insert all the elements of one array into another, at any index. Ln 225

--- index.js
+++ index.js
@@ -204,13 +204,26 @@ Having more high cards remaining in the deck favors the player. Each card is ass
 // --------------------------------------------------------
 /*Define a function, copyMachine which takes arr (an array) and num (a number) as arguments. The function is supposed to return a new array made up of num copies of arr. We have done most of the work for you, but it doesn't work quite right yet. Modify the function using spread syntax so that it works correctly (hint: another method we have already covered might come in handy here!). */ 
 
-function copyMachine(arr, num){
-let newArray = [];
-  while(num >= 1){
+// function copyMachine(arr, num){
+// let newArray = [];
+//   while(num >= 1){
   
-    newArray.push([...arr])
-    num--;
-  }
-  return newArray
-};
-console.log(copyMachine([true, false, true], 2));
+//     newArray.push([...arr])
+//     num--;
+//   }
+//   return newArray
+// };
+// console.log(copyMachine([true, false, true], 2));
+
+// ----------------------------------------------------
+
+/* defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun']. */ 
+
+
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning', ... fragment, 'is', 'fun']; // Change this line
+  return sentence;
+}
+
+console.log(spreadOut());
